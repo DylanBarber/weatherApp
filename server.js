ccd17f22726e2bae67f775b829c22fb024f948e4
Dylan Barber <DylanBarber1@Outlook.com>
2019-08-23 16:38:11 -0400

    back end is live. Created GET route at /apitest/(city name)

--- server.js
+++ server.js
@@ -2,28 +2,35 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path')
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// const fetchTest = async () => {
-//     // let locationFetch = await fetch(`http://open.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST_KEY}&city=${this.state.inputText}`)
-//     let locationData = await locationFetch.json()
-//     let latitude = locationData.results[0].locations[0].latLng.lat
-//     let longitude = locationData.results[0].locations[0].latLng.lng
-//     console.log(latitude, longitude);
-// }
+const fetch = require('node-fetch')
+const dotenv = require('dotenv')
+dotenv.config()
+const fetchTest = async (city) => {
+    let locationFetch = await fetch(`http://open.mapquestapi.com/geocoding/v1/address?key=${process.env.MAPQUEST_KEY}&city=${city}`)
+    let locationData = await locationFetch.json()
+    let latitude = locationData.results[0].locations[0].latLng.lat
+    let longitude = locationData.results[0].locations[0].latLng.lng
+    let weatherFetch = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${latitude},${longitude}`)
+    let weatherData = await weatherFetch.json()
+    return weatherData
+}
 
 app.use(express.static(path.join(__dirname, 'client/build')))
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
+// // create a GET route
+// app.get('/express_backend', (req, res) => {
+//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+//API return
+app.get('/apitest/:city', async (req, res) => {
+  const locationData = await fetchTest(req.params.city)
+  res.send(locationData)
+})
+
+//Catchall handler
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
